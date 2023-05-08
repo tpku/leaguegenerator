1eb33c39679be460c1247187cc8a711c10393831
tpku <tpku@icloud.com>
2023-05-08 09:36:26 +0200

    Updated styling MainWrapper component

--- src/components/MainWrapper/MainWrapper.jsx
+++ src/components/MainWrapper/MainWrapper.jsx
@@ -3,13 +3,18 @@ import styled from "styled-components";
 const StyledMain = styled.main`
     width: 100%;
     min-height: 100vh;
+    /* margin-top: 10vh; */
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
 `;
 
 const MainWrapper = (props) => {
     const { children } = props;
     return (
         <>
-            <StyledMain>{children}</StyledMain>;
+            <StyledMain>{children}</StyledMain>
         </>
     );
 };
